12aee2b2c7885d238ef7c7c48afd7b48cd298bc6
Liliia Baieva <555blv@gmail.com>
2023-03-18 13:29:33 +0100

    make PrivateHouse class

---
+++ src/Task1_Hause/PrivateHouse.java
@@ -0,0 +1,10 @@
+package Task1_Hause;
+
+public class PrivateHouse extends House{
+  public PrivateHouse (String city, String street, int houseNumber){
+    super(city, street,houseNumber);
+
+  }
+
+
+}
