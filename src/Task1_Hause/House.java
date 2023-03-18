e33e46b1872c93889fa932b9e644fc42f315e041
Liliia Baieva <555blv@gmail.com>
2023-03-18 13:15:24 +0100

    make constructor, getters and setters in class House

---
+++ src/Task1_Hause/House.java
@@ -0,0 +1,35 @@
+package Task1_Hause;
+
+public class House {
+  private String city;
+  private String street;
+  private int houseNumber;
+
+  public House(String city, String street, int houseNumber){
+    this.city = city;
+    this.street = street;
+    this.houseNumber = houseNumber;
+  }
+
+  public String getCity() {
+    return city;
+  }
+  public String getStreet() {
+    return street;
+  }
+  public int getHouseNumber() {
+    return houseNumber;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+  public void setStreet(String street) {
+    this.street = street;
+  }
+  public void setHouseNumber(int houseNumber) {
+    this.houseNumber = houseNumber;
+  }
+
+
+}
